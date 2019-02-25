79510cc7e96f0e0eec1507195aee4723cde8d9f2
FabienBasset <fab.ounet.basset@gmail.com>
2019-02-25 17:19:53 +0100

    try again !

---
+++ ProjetThematique/Data/Migrations/20190225135555_ProjetThematique.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetThematique.Data.Migrations
+{
+    public partial class ProjetThematique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
